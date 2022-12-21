2994dc29b861b83b02a84cad2026754e1fd39a10
Churchill Selma <tech1@sumomedia.co>
2022-12-21 11:47:59 +0800

    Revert direct edit on database migrationsn for orders table

--- database/migrations/2022_09_03_105317_create_orders_table.php
+++ database/migrations/2022_09_03_105317_create_orders_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('transaction_id');
-            $table->string('payment_gateway'); // Use PaymentGateway enum
+            $table->string('payment_intent_id');
             $table->string('firstname');
             $table->string('lastname');
             $table->string('phone_number');
@@ -26,7 +25,6 @@ return new class extends Migration
             $table->string('post_code')->nullable();
             $table->mediumText('remarks');
             $table->integer('total');
-            $table->boolean('is_verified')->default(false);
             $table->timestamps();
         });
     }
