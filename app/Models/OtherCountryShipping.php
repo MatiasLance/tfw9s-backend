7bf032b098ec5ea1f9f1d0224befb727a141ecb4
lance <landero011898@gmail.com>
2023-01-13 14:51:33 +0800

    Backend Tweak

--- app/Models/OtherCountryShipping.php
+++ app/Models/OtherCountryShipping.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Modules\Currency\Traits\HandlesCurrency;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OtherCountryShipping extends Model
 {
     use HasFactory;
+    use HandlesCurrency;
+
     protected $fillable = [
         'name',
         'country',
@@ -16,4 +20,56 @@ class OtherCountryShipping extends Model
         'registered_value',
         'express_value'
     ];
+
+    public function shippingValue(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($val) => $this->toPrice($val),
+            set: fn ($val) => $this->toCent($val),
+        );
+    }
+
+    public function insuranceValue(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($val) => $this->toPrice($val),
+            set: fn ($val) => $this->toCent($val),
+        );
+    }
+
+    public function registeredValue(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($val) => $this->toPrice($val),
+            set: fn ($val) => $this->toCent($val),
+        );
+    }
+
+    public function expressValue(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($val) => $this->toPrice($val),
+            set: fn ($val) => $this->toCent($val),
+        );
+    }
+
+    public function shippingCentPrice(): int
+    {
+        return $this->getAttributes()['shipping_value'];
+    }
+
+    public function insuranceCentPrice(): int
+    {
+        return $this->getAttributes()['insurance_value'];
+    }
+
+    public function registeredCentPrice(): int
+    {
+        return $this->getAttributes()['registered_value'];
+    }
+
+    public function expressCentPrice(): int
+    {
+        return $this->getAttributes()['express_value'];
+    }
 }
