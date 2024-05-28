a8bc643b96a8856eb10f002941ab60e50e7d0834
Jhuvizier Yamaguchi <tech18@sumomedia.co>
2024-05-29 07:29:02 +0800

    registration initial commit

--- database/migrations/2024_03_21_084025_create_teams_table.php
+++ database/migrations/2024_03_21_084025_create_teams_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->foreignId('registration_id');
             $table->foreignId('field_id')->nullable();
             $table->foreignId('agegroup_id')->nullable();
             $table->foreignId('event_id')->nullable();
